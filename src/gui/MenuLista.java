e89b48024ab9eb1fa86869e024be6135d1c7a25d
Daniel Vrána <vrad00@vse.cz>
2017-11-07 10:10:57 +0100

    gui done

--- src/gui/MenuLista.java
+++ src/gui/MenuLista.java
@@ -7,12 +7,17 @@ package gui;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 import logika.Hra;
 import logika.IHra;
 import main.Main;
@@ -35,6 +40,7 @@ public class MenuLista extends MenuBar {
     private void init()
     {
         Menu novySoubor = new Menu("Adventura");
+        Menu about = new Menu ("Help");
         
         MenuItem novaHra = new MenuItem("Nová hra");
         //new ImageView(new Image(Main.class.getResourceAsStream("/zdroje/ikona.png")))
@@ -43,7 +49,12 @@ public class MenuLista extends MenuBar {
         
         novySoubor.getItems().addAll(novaHra, konecHry);
         
-        this.getMenus().addAll(novySoubor);
+        MenuItem oProgramu = new MenuItem("O programu");
+        MenuItem napoveda = new MenuItem("Napoveda");
+        
+        about.getItems().addAll(oProgramu, napoveda);
+        
+        this.getMenus().addAll(novySoubor, about);
         
         konecHry.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -63,6 +74,35 @@ public class MenuLista extends MenuBar {
                 main.getCentralText().setText(hra.vratUvitani());
             }
         }));
+        
+        oProgramu.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("O programu");
+                alert.setContentText("Verze 1.0.0");
+                alert.setHeaderText("Adventura");
+                alert.show();
+            }
+        });
+        
+        napoveda.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                Stage stage = new Stage();
+                stage.setTitle("Napoveda");
+                
+                WebView webView = new WebView();
+                
+                webView.getEngine().load(Main.class.getResource("/zdroje/napoveda.html").toExternalForm());
+                
+                stage.setScene(new Scene(webView, 500,500));
+                stage.show();
+            }
+        });
+        
         
     }
 }
